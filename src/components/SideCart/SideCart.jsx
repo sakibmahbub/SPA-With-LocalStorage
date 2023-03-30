f48263af6232bf9d4c53d65e6f36691821d82d45
sakibmahbub <arafatsakib48@gmail.com>
2023-03-30 17:11:45 +0600

    get time from localstorage in cart

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SideCart = ({ watchTime }) => {
+  const [time, setTime] = useState();
+  useEffect(() => {
+    const getTime = localStorage.getItem("watchTime");
+    setTime(getTime);
+  }, [watchTime]);
   return (
-    <div className="border-2 border-zinc-400 p-10 rounded-lg">
+    <div className="w-72 border-2 border-zinc-400 p-10 rounded-lg">
       <h1 className="text-2xl font-bold text-center">My Cart</h1>
       <div className="mt-5 text-center">
         <p>Total Watch Time</p>
         <input type="text" value={0} disabled />
       </div>
-      <h5 className="text-center mt-3  text-lg">Add Break Time</h5>
-      <div className="text-center mt-4">
-        <button className="w-25 btn-circle btn btn-success">15</button>
-        <button className="w-25 btn-circle btn btn-info ml-2">20</button>
-        <button className="w-25 btn-circle btn btn-danger ml-2">25</button>
+      <h5 className="text-center mt-4  text-lg">Add Break Time</h5>
+      <div className="text-center mt-5">
+        <button className="w-25 btn-circle btn btn-primary">15</button>
+        <button className="w-25 btn-circle btn btn-primary ml-2">20</button>
+        <button className="w-25 btn-circle btn btn-primary ml-2">25</button>
       </div>
     </div>
   );
